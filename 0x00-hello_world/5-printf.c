c9f020b19e88a4dca29aebe99ed335dc7777f46e
060qiniso <060qiniso@gmail.com>
2023-06-15 12:53:30 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
+/**
+ *main - use function to print a line
+ *
+ * Return:0 (success)
+ */
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
