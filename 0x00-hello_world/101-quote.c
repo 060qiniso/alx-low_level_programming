720200d69451fb61079e898e576942baa22be319
060qiniso <060qiniso@gmail.com>
2023-06-15 14:11:09 -0700

    Itel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+ *main - use write function
+ *
+ *Return 0 (success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 76);
+	return (1);
+}
