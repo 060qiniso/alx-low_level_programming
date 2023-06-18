c3cc7c232f291fb9487e0533fb5214cf34bebbc6
060qiniso <060qiniso@gmail.com>
2023-06-18 11:29:43 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of single-digit numbers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
