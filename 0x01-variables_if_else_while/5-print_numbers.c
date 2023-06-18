310b89dbd63ad6550ddb61d5a6d62792f60dd8dd
060qiniso <060qiniso@gmail.com>
2023-06-18 05:57:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program print numbers of base 10 starting from 0
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar ('\n');
+	return (0);
+}
