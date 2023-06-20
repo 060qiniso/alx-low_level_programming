c8624aa25f4ad57e16ae5494a0627759515d1ddb
060qiniso <060qiniso@gmail.com>
2023-06-20 11:47:29 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include"main.h"
 
 /**
- * main - function that prints the alphabet, in lowercase
- *
- * Return: Always 0
+ * print_alphabet - function that prints the alphabet, in lowercase
  */
 void print_alphabet(void)
 {
@@ -14,4 +12,4 @@ void print_alphabet(void)
 		_putchar(alph);
 	}
 	_putchar('\n');
-}	
+}
