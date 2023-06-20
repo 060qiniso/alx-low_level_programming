c3b1694aabe332afb5dfd77a13d48fdef4d06c7a
060qiniso <060qiniso@gmail.com>
2023-06-20 14:44:04 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,14 @@ int print_last_digit(int n)
 	int q;
 
 	if (n < 0)
-	{
-		n = -n;
-		q = n % 10;
-	}
+	n = -n;
+
+	q = n % 10;
+
 	if (q < 0)
-	{
 		q = -q;
-		_putchar(q + '0');
-	}
+
+	_putchar(q + '0');
+
 	return (q);
 }
