faf973e219621649f79b69643d88ca08210ca84e
060qiniso <060qiniso@gmail.com>
2023-06-20 15:15:47 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int q, r;
+
+	q = 0;
+
+	while (q < 24)
+	{
+		r = 0;
+		while (r < 60)
+		{
+			_putchar((q / 10) + '0');
+			_putchar((q % 10) + '0');
+			_putchar(':');
+			_putchar((r % 10) + '0');
+			_putchar((r % 10) + '0');
+			_putchar('\n');
+			r++;
+		}
+		q++;
+	}
+}
