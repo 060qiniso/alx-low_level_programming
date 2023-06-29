b62ed49e546bc3c9d4539555cfd1d50c82d4deac
060qiniso <060qiniso@gmail.com>
2023-06-29 11:44:10 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - function that encodes a string using rot13
+ * @s: pointer to string params
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int q;
+	int r;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (q = 0; s[q] != '\0'; q++)
+	{
+		for (r = 0; r < 52; r++)
+		{
+			if (s[q] == data1[r])
+			{
+				s[q] = datarot[r];
+				break;
+			}
+		}
+	}
+	return (s);
+}
